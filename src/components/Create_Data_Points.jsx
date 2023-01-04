ecef78bf0f046137534a063ef9ea851c4e37e4c2
Hassaan1814 <hassaanjaved82@triconv.com>
2023-01-04 13:09:53 +0500

    adding styles

---
+++ src/components/Create_Data_Points.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./createdatapoints.css";
+const Create_Data_Points = () => {
+  return (
+    <>
+      <div>Create_Data_Points</div>
+    </>
+  );
+};
+
+export default Create_Data_Points;

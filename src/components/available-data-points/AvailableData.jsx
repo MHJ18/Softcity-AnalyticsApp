f2924468e5a78d5d1cb50bb844dff28460e44c0b
Hssaan1814 <hassaanjaved82@triconv.com>
2023-01-12 00:13:40 +0500

    component available data

--- src/components/available-data-points/AvailableData.jsx
+++ src/components/available-data-points/AvailableData.jsx
@@ -1,10 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
 import "./availabledata.css";
-const AvailableData = ({ func }) => {
+const AvailableData = () => {
+  const [check, setcheck] = useState(false);
+
+  const onclick = function (e) {
+    e.preventDefault();
+    console.log(e.target.closest(".box-1").textContent);
+  };
   return (
     <>
       <div className="data_point-right">
-        <div className="div"></div>
+        <div className="div">
+          <div className="middle">
+            <label onClick={onclick}>
+              <div className="front-end box">
+                <div className="select_box-1">
+                  <p className="box-text">Front-end</p>
+                </div>
+                <div className="select_box-description">?</div>
+              </div>
+              <input
+                type="checkbox"
+                name="checkbox"
+                onChange={() => {
+                  setcheck(!check);
+                }}
+              />
+            </label>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/create-data-points/Form.jsx
+++ src/components/create-data-points/Form.jsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./createdatapoints.css";
 import { BsArrowRightShort } from "react-icons/bs";
 import { IoMdAddCircle } from "react-icons/io";
